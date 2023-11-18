a247c9d5da5d829fa8cc35443cbe76e54569c788
Bruno Grassano <bgrassano@fi.uba.ar>
2023-11-18 17:21:27 -0300

    Add logout option to sidebar

--- src/components/SideBar/sideBar.jsx
+++ src/components/SideBar/sideBar.jsx
@@ -1,10 +1,33 @@
-import { Avatar, Drawer, List, Stack, Toolbar } from "@mui/material";
+import { useState } from "react";
+import { Avatar, Drawer, List, Stack, Toolbar, Tooltip, Menu, Typography,MenuItem   } from "@mui/material";
 import appRoutes from "../../routes/ReactRoutes";
 import SidebarItem from "./sideBarItem";
 import  useUser  from "../../stores/useUser";
 import {UserLogout} from "../../services/userService"
+import {Blue900} from "../../resources/colors/colors"
+
+
+function mapMenuOptions() {
+    return (
+        <MenuItem key={"logout"} onClick={UserLogout}>
+            <Typography textAlign="center">Cerrar sesion</Typography>
+        </MenuItem>
+    )
+}
+
 
 const Sidebar = () => {
+
+    const [anchorElUser, setAnchorElUser] = useState(null);
+  
+    const handleOpenUserMenu = (event) => {
+      setAnchorElUser(event.currentTarget);
+    };
+  
+    const handleCloseUserMenu = () => {
+      setAnchorElUser(null);
+    };
+
     const { userInfo } = useUser()
     return (
         <Drawer
@@ -19,24 +42,46 @@ const Sidebar = () => {
                 backgroundColor: "white",
                 color: "#1A1F36",
             }}
-        }
-        
-        >
+        }>
         <List disablePadding>
             <Toolbar sx={{ marginBottom: "20px" }}>
-            <Stack
-                sx={{ width: "100%" }}
-                direction="row"
-                justifyContent="left"
-                onClick ={ () => {UserLogout()}}
-            >
-                <Avatar/>
-                <text
-                    style={{
-                        marginLeft: "30px",
-                        }}
-                > {userInfo.userName} </text>
-            </Stack>
+                <Tooltip title="Acciones de usuario">
+                    <Stack
+                        sx={{ width: "100%" }}
+                        direction="row"
+                        justifyContent="left"
+                        alignItems="center"
+                        onClick={handleOpenUserMenu}
+                        style={{cursor:"pointer"}}
+                    >
+                        <Avatar/>
+                        <text
+                            style={{
+                                marginLeft: "30px",
+                                color: Blue900,
+                                fontWeight: "bold"
+                                }}
+                        > {userInfo.userName} </text>
+                    </Stack>
+                </Tooltip>
+                <Menu
+                    sx={{ mt: '45px' }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                >
+                    {mapMenuOptions()}
+                </Menu>
             </Toolbar>
             {appRoutes.map((route, index) => (
             route.sidebarProps ? (
