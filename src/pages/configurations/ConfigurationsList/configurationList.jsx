c04f3f26465afe04114d84dc11b2a3774fd98665
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-03-21 21:44:01 -0300

    fetch data when delete

--- src/pages/configurations/ConfigurationsList/configurationList.jsx
+++ src/pages/configurations/ConfigurationsList/configurationList.jsx
@@ -9,15 +9,20 @@ import { Box, Button, ListItemButton, CircularProgress } from '@mui/material';
 import { CreateConfigurations } from '../createConfigurations/createConfigurations';
 import { ConfigurationListPresenter } from '../../../presenters/configurationListPresenter';
 import { ConfigurationContext } from '../../../providers/configProvider';
+//import useUser from '../../../stores/useUser';
+//import { UserContext } from '../../../stores/userContext';
 
-function getDeleteConfigModalContent(configuration, presenter){
+function getDeleteConfigModalContent(configuration, presenter, handleDelete){
     return(
         <div>
             <h4>
                 ¿Estas seguro que quieres eliminar {configuration.Name}?
             </h4>
             <div>
-                <Button variant="contained" color='error' onClick={()=>presenter.onDeleteConfig(configuration.Id)} sx={{marginX:5}}>
+                <Button variant="contained" color='error' onClick={()=> {
+                    presenter.onDeleteConfig(configuration.Id)
+                    handleDelete(configuration.Id)
+                    }} sx={{marginX:5}}>
                     Si
                 </Button>
                 <Button variant="contained" sx={{marginX:5}} onClick={presenter.onCancelDeleteConfig}>
@@ -28,12 +33,19 @@ function getDeleteConfigModalContent(configuration, presenter){
     )
 }
 
+
 export const ConfigurationsList = () => {
     const presenter = new ConfigurationListPresenter()
     const {currentConfigID, selectConfig} = useContext(ConfigurationContext);
-
+    //const {userInfo} = useUser()
     const [configurations, setConfigurations] = useState([]);
     const [isLoading, setLoading] = useState(true)
+    const isAdmin = true
+    
+    const handleDelete = (id) => {
+        const updatedData = configurations.filter((item) => item.Id !== id);
+        setConfigurations(updatedData);
+    };
 
     const fetchConfigData = useCallback(async( func)=> {
         const data = await func();
@@ -54,7 +66,7 @@ export const ConfigurationsList = () => {
                     justifyContent:"space-between"}}>
 
                 <span style={{ fontWeight: 'bold' }}>Nombre de configuracion</span>
-                <span style={{ fontWeight: 'bold' }}>Acción</span>
+                <span style={{ fontWeight: 'bold', marginRight: 20}}>Acción</span>
             </Box>
             <Line></Line>
             {isLoading?
@@ -84,11 +96,12 @@ export const ConfigurationsList = () => {
                                         >
                                             usar
                                         </Button>
-                                        <IconButton aria-label="trash" onClick={() => {try {
-                                            presenter.onDeleteConfigPressed(getDeleteConfigModalContent( configuration, presenter ))}
-                                            finally {fetchConfigData()}}}>
-                                            <DeleteForeverIcon color='error'/>
-                                        </IconButton>
+                                        {isAdmin ?
+                                            <IconButton aria-label="trash" onClick={() => {
+                                                presenter.onDeleteConfigPressed(getDeleteConfigModalContent( configuration, presenter, handleDelete))
+                                                }}>
+                                                <DeleteForeverIcon color='error'/>
+                                            </IconButton> : <></>}
                                     </div>
                                 }
                             >
@@ -98,7 +111,7 @@ export const ConfigurationsList = () => {
                     ))}
                 </List>
             }
-            <Button variant="outlined" color="warning" sx={{marginTop:"5%"}}>Agregar configuración</Button>
+            {isAdmin ? <Button variant="outlined" sx={{marginTop:"5%"}}>Agregar configuración</Button> :<></>}
         </Box>
     );
 }
