18c5612fa7cb032b4ee34f6901f7cbbbb5d3d278
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-03-22 12:24:42 -0300

    Add route series

---
+++ src/pages/series/series.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+
+export const Series = () => {
+
+    return (
+        <Box>
+            <h1> Series </h1>        
+        </Box>
+    );
+}
