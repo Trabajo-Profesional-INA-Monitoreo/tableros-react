c73d2bbea470d7aa5e3a8d7a469174898c1468a8
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-03-29 13:50:06 -0300

    Add seriesCard

--- src/pages/series/series.jsx
+++ src/pages/series/series.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, {useContext, useState} from 'react';
 import {Box, Container, TextField, Button, Select, FormControl, InputLabel, MenuItem} from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import Line from '../../components/line/line';
+import { ConfigurationContext } from '../../providers/configProvider';
+import SeriesCard from '../../components/seriesCard/seriesCard';
 
 export const Series = () => {
     const [idSerie, setIdSerie] = useState('');
@@ -11,13 +13,20 @@ export const Series = () => {
     const [idEstacion, setIdEstacion] = useState('');
     const [tipoSerie, setTipoSerie] = useState('');
     const [variable, setVariable] = useState('');
-    const [red, setRed] = useState('');
+    const {currentConfigName} = useContext(ConfigurationContext);
 
+    const series = [
+        {id: 1, nameVar: "variable1", estacion: "Palmira", CantErrores: 3, tiempoDeRetraso: "01:23:34", procedimiento: "algo", tipo: "Observada"},
+        {id: 2, nameVar: "variable2", estacion: "Santa Rosa",CantErrores: 0, tiempoDeRetraso: "02:56:34", procedimiento: "algo2", tipo: "Simulada"},
+        {id: 3, nameVar: "variable3", estacion: "Paraná",CantErrores: 5, tiempoDeRetraso: "00:23:34", procedimiento: "algo3", tipo: "Procesada"},
+        {id: 4, nameVar: "variable4", estacion: "Paraná",CantErrores: 5, tiempoDeRetraso: "00:23:34", procedimiento: "algo3", tipo: "Procesada"},
+    ]
     async function aplicarFiltros(){}
 
     return (
         <div style={{maxWidth: "100%"}}>
             <h1> Series </h1>
+            <h4>Configuración actual: {currentConfigName}</h4>
             <Line/>
             <Container sx={{display:"flex", flexFlow:"wrap", justifyContent:"center"}}>
                 <Grid items>
@@ -72,16 +81,6 @@ export const Series = () => {
                                 sx={{margin:2}}
                             />
                             <TextField 
-                                label = "Red"
-                                type = "text"
-                                placeholder = "Red"
-                                name = "Red"
-                                className={"inputStyle"}
-                                value={red}
-                                onChange = {(event) => setRed(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
                                 label = "Id estacion"
                                 type = "text"
                                 placeholder = "Id estacion"
@@ -102,7 +101,7 @@ export const Series = () => {
                                 >
                                     <MenuItem value={10}>Pronosticada</MenuItem>
                                     <MenuItem value={20}>Observada</MenuItem>
-                                    <MenuItem value={30}>Redundante</MenuItem>
+                                    <MenuItem value={30}>Simulada</MenuItem>
                                 </Select>
                             </FormControl>
                 </Grid>
@@ -115,6 +114,21 @@ export const Series = () => {
                     </Button>
                 </div>
             </Container>
+            <Container>
+                    <Grid
+                            container
+                            spacing={5}
+                            style={{justifyContent: "center", alignItems:"center"}}
+                        >
+                            {series.map((serie, index) => (
+                                <Grid item key={index}>
+                                    <SeriesCard serieData={serie}/>
+                                </Grid>
+                                ))
+                            }
+                        </Grid>
+        </Container>
+
         </div>
     );
 }
