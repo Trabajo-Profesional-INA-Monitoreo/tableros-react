dfab90a7f2017f3a66b7881012b02ce9e746a446
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-04-05 14:11:00 -0300

    fix pagenumber

--- src/components/pagination/paginationComponent.jsx
+++ src/components/pagination/paginationComponent.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 
 const PaginationComponent = ({ page, totalPages, setPage}) => {
-
+    page = page+1
     const handlePageChange = (newPage) => {
         setPage(newPage);
     };
 
     const handlePrevPage = () => {
         if (page > 1) {
-        handlePageChange(page - 1);
+            handlePageChange(page - 1);
         }
     };
 
     const handleNextPage = () => {
         if (page < totalPages) {
-        handlePageChange(page + 1);
+            handlePageChange(page + 1);
         }
     };
 
     const renderPageNumbers = () => {
         const pageNumbers = [];
 
-        for (let i = 0; i <= totalPages; i++) {
-        pageNumbers.push(i);
+        for (let i = 1; i <= totalPages; i++) {
+            pageNumbers.push(i);
         }
 
         return pageNumbers.map((number) => {
-        if (number === 1) {
-            return (
-            <button key={number} onClick={() => handlePageChange(number)}>
-                {number}
-            </button>
-            );
+            if (number === 1) {
+                return (
+                    <button key={number} onClick={() => handlePageChange(number)}>
+                        {number}
+                    </button>
+                );
         } else if (number === totalPages) {
             return (
-            <button key={number} onClick={() => handlePageChange(number)}>
-                ...{number}
-            </button>
+                <button key={number} onClick={() => handlePageChange(number)}>
+                    ...{number}
+                </button>
             );
         } else if (number === page) {
             return (
-            <button key={number} className="active">
-                {number}
-            </button>
+                <button key={number} className="active">
+                    {number}
+                </button>
             );
         } else {
             return (
-            <button key={number} onClick={() => handlePageChange(number)}>
-                {number}
-            </button>
+                <button key={number} onClick={() => handlePageChange(number)}>
+                    {number}
+                </button>
             );
         }
         });
@@ -56,7 +56,7 @@ const PaginationComponent = ({ page, totalPages, setPage}) => {
 
     return (
         <div>
-            {page>0?<button onClick={handlePrevPage}>Previous Page</button>:<></>}
+            {page>1?<button onClick={handlePrevPage}>Previous Page</button>:<></>}
             {renderPageNumbers()}
             {page<totalPages?<button onClick={handleNextPage}>Next Page</button>:<></>}
         </div>
--- src/pages/series/series.jsx
+++ src/pages/series/series.jsx
@@ -85,94 +85,94 @@ export const Series = () => {
                     }}
                 />
                 :<>
-            <Container sx={{display:"flex", flexFlow:"wrap", justifyContent:"center"}}>
-                <Grid items>
-                            <TextField 
-                                label = "ID Serie"
-                                type = "text"
-                                placeholder = "Seleccione un ID de serie"
-                                name = "IdSerie"
-                                className={"inputStyle"}
-                                value={idSerie}
-                                onChange = {(event) => setIdSerie(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
-                                label = "Desde"
-                                type = "text"
-                                placeholder = "Desde"
-                                name = "Desde"
-                                className={"inputStyle"}
-                                value={desde}
-                                onChange = {(event) => setDesde(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
-                                label = "Hasta"
-                                type = "text"
-                                placeholder = "Hasta"
-                                name = "Hasta"
-                                className={"inputStyle"}
-                                value={hasta}
-                                onChange = {(event) => setHasta(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
-                                label = "Variable"
-                                type = "text"
-                                placeholder = "Seleccione variable"
-                                name = "variable"
-                                className={"inputStyle"}
-                                value={variable}
-                                onChange = {(event) => setVariable(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
-                                label = "Procedimiento"
-                                type = "text"
-                                placeholder = "Procedimiento"
-                                name = "procedimiento"
-                                className={"inputStyle"}
-                                value={procedimiento}
-                                onChange = {(event) => setProcedimiento(event.target.value)}
-                                sx={{margin:2}}
-                            />
-                            <TextField 
-                                label = "Id estacion"
-                                type = "text"
-                                placeholder = "Id estacion"
-                                name = "idEstacion"
-                                className={"inputStyle"}
-                                value={idEstacion}
-                                onChange = {(event) => setIdEstacion(event.target.value)}
-                                sx={{margin: 2}}
-                            />
-                            <FormControl sx={{ m: 1, minWidth: 150, marginInline: 2, marginTop:2}}>
-                                <InputLabel id="idEstacion">Tipo de serie</InputLabel>
-                                <Select
-                                    labelId="tipoDeSerie"
-                                    id="demo-simple-select"
-                                    value={tipoSerie}
-                                    label="Tipo de serie"
-                                    onChange={(event) => setTipoSerie(event.target.value)}
-                                >
-                                    <MenuItem value={10}>Pronosticada</MenuItem>
-                                    <MenuItem value={20}>Observada</MenuItem>
-                                    <MenuItem value={30}>Simulada</MenuItem>
-                                </Select>
-                            </FormControl>
-                </Grid>
-                <div>
-                    <Button variant="contained" onClick={aplicarFiltros} sx={{margin: 2, marginInline:5}}>
-                        Aplicar filtros
-                    </Button>
-                    <Button variant="contained" onClick={aplicarFiltros} sx={{margin: 2, marginInline:5}}>
-                        Descargar datos
-                    </Button>
-                </div>
-            </Container>
-            <Container>
-                    <Grid
+                    <Container sx={{display:"flex", flexFlow:"wrap", justifyContent:"center"}}>
+                        <Grid items>
+                                    <TextField 
+                                        label = "ID Serie"
+                                        type = "text"
+                                        placeholder = "Seleccione un ID de serie"
+                                        name = "IdSerie"
+                                        className={"inputStyle"}
+                                        value={idSerie}
+                                        onChange = {(event) => setIdSerie(event.target.value)}
+                                        sx={{margin:2}}
+                                    />
+                                    <TextField 
+                                        label = "Desde"
+                                        type = "text"
+                                        placeholder = "Desde"
+                                        name = "Desde"
+                                        className={"inputStyle"}
+                                        value={desde}
+                                        onChange = {(event) => setDesde(event.target.value)}
+                                        sx={{margin:2}}
+                                    />
+                                    <TextField 
+                                        label = "Hasta"
+                                        type = "text"
+                                        placeholder = "Hasta"
+                                        name = "Hasta"
+                                        className={"inputStyle"}
+                                        value={hasta}
+                                        onChange = {(event) => setHasta(event.target.value)}
+                                        sx={{margin:2}}
+                                    />
+                                    <TextField 
+                                        label = "Variable"
+                                        type = "text"
+                                        placeholder = "Seleccione variable"
+                                        name = "variable"
+                                        className={"inputStyle"}
+                                        value={variable}
+                                        onChange = {(event) => setVariable(event.target.value)}
+                                        sx={{margin:2}}
+                                    />
+                                    <TextField 
+                                        label = "Procedimiento"
+                                        type = "text"
+                                        placeholder = "Procedimiento"
+                                        name = "procedimiento"
+                                        className={"inputStyle"}
+                                        value={procedimiento}
+                                        onChange = {(event) => setProcedimiento(event.target.value)}
+                                        sx={{margin:2}}
+                                    />
+                                    <TextField 
+                                        label = "Id estacion"
+                                        type = "text"
+                                        placeholder = "Id estacion"
+                                        name = "idEstacion"
+                                        className={"inputStyle"}
+                                        value={idEstacion}
+                                        onChange = {(event) => setIdEstacion(event.target.value)}
+                                        sx={{margin: 2}}
+                                    />
+                                    <FormControl sx={{ m: 1, minWidth: 150, marginInline: 2, marginTop:2}}>
+                                        <InputLabel id="idEstacion">Tipo de serie</InputLabel>
+                                        <Select
+                                            labelId="tipoDeSerie"
+                                            id="demo-simple-select"
+                                            value={tipoSerie}
+                                            label="Tipo de serie"
+                                            onChange={(event) => setTipoSerie(event.target.value)}
+                                        >
+                                            <MenuItem value={10}>Pronosticada</MenuItem>
+                                            <MenuItem value={20}>Observada</MenuItem>
+                                            <MenuItem value={30}>Simulada</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                        </Grid>
+                        <div>
+                            <Button variant="contained" onClick={aplicarFiltros} sx={{margin: 2, marginInline:5}}>
+                                Aplicar filtros
+                            </Button>
+                            <Button variant="contained" onClick={aplicarFiltros} sx={{margin: 2, marginInline:5}}>
+                                Descargar datos
+                            </Button>
+                        </div>
+                    </Container>
+                    <Container>
+                        <Grid
                             container
                             spacing={5}
                             style={{justifyContent: "center", alignItems:"center"}}
@@ -183,13 +183,12 @@ export const Series = () => {
                                 </Grid>
                                 ))
                             }
-                        </Grid>
-                    
-        </Container>
-        </>
-        }
-        <PaginationComponent page={page} totalPages={totalPages} setPage={setPage}/>
-        <SerieModal open={openModal} handleClose={handleCloseModal}/>   
+                        </Grid>     
+                </Container>
+                </>
+                }
+            <PaginationComponent page={page} totalPages={totalPages} setPage={setPage}/>
+            <SerieModal open={openModal} handleClose={handleCloseModal}/>   
         </div>
     );
 }            
