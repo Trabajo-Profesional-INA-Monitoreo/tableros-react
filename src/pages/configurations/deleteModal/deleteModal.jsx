713c97cc18c8b08b5a396b20d4443b40f9284d41
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-05-01 17:35:07 -0300

    fix style

--- src/pages/configurations/deleteModal/deleteModal.jsx
+++ src/pages/configurations/deleteModal/deleteModal.jsx
@@ -8,8 +8,6 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: '50%',
-    height: '20%',
     bgcolor: 'background.paper',
     boxShadow: 24,
     display: 'flex',
