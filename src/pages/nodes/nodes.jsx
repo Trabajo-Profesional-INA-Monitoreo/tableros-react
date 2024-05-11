ad099d068c1214990bd73c0f0beb8066ad59d994
Stephanieizquierdo <sizquierdo@fi.uba.ar>
2024-05-11 20:24:41 -0300

    Fix formateo time + null management

--- src/pages/nodes/nodes.jsx
+++ src/pages/nodes/nodes.jsx
@@ -6,6 +6,20 @@ import { CurrentConfiguration } from '../../components/currentConfiguration/curr
 import { NodePresenter } from '../../presenters/nodePresenter';
 import Line from '../../components/line/line';
 
+
+function formatDate(isoDate) {
+    const date = new Date(isoDate);
+    const offset = -3 * 60 * 60 * 1000; // GMT-3 offset in milliseconds
+    const gmt3Date = new Date(date.getTime() + offset);
+    const day = gmt3Date.getDate().toString().padStart(2, '0');
+    const month = (gmt3Date.getMonth() + 1).toString().padStart(2, '0');
+    const year = gmt3Date.getFullYear();
+    const hours = gmt3Date.getHours().toString().padStart(2, '0');
+    const minutes = gmt3Date.getMinutes().toString().padStart(2, '0');
+    const seconds = gmt3Date.getSeconds().toString().padStart(2, '0');
+    return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+}
+
 export const Nodes = () => {
     
     const presenter = new NodePresenter();
@@ -32,8 +46,8 @@ export const Nodes = () => {
             {isLoading ? null : nodes.map(node => 
                 <InformativeCard 
                     title={node.NodeName+' | '+node.NodeId}
-                    subtitle={node.StreamsCount + (Number(node.StreamsCount) > 1 ? ' series' : ' serie')}
-                    heading1={node.LastUpdate}
+                    subtitle={`${node.StreamsCount + (Number(node.StreamsCount) > 1 ? ' series' : ' serie')} ${node.MainStreamId ? (' | Serie principal: ' + node.MainStreamId) :"" }`}
+                    heading1={node.LastUpdate && formatDate(node.LastUpdate)}
                     heading2={node.ErrorCount}
                     heading3={node.TotalWaterLevels>0? (node.AlertWaterLevels / node.TotalWaterLevels)*100 : 0}
                     subheading1={'Última actualización'}
