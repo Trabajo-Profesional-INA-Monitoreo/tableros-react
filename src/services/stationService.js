5a085f11e4abd8d94c5d36b39d79f7e5c2d55a21
Adrian Romero <36236300+adrianromero99@users.noreply.github.com>
2024-05-12 12:21:56 -0300

    fix: hot-fix Add configuration ID in filters

--- src/services/stationService.js
+++ src/services/stationService.js
@@ -1,15 +1,16 @@
 import { BASE_URL } from "../utils/service";
+import { getConfigurationID } from "../utils/storage";
 
 export default class stationService {
 
     async getStations(configurationId) {
-        const response = await fetch(BASE_URL+"/series/estaciones?configurationId="+configurationId);
+        const response = await fetch(`${BASE_URL}/series/estaciones?configurationId=${getConfigurationID()}`);
         const res = await response.json();
         return res;
     }
 
     async getAllStationsNames() {
-        const response = await fetch(`${BASE_URL}/filtro/estaciones`);
+        const response = await fetch(`${BASE_URL}/filtro/estaciones?configurationId=${getConfigurationID()}`);
         const res = await response.json();
         return res;
     }
--- src/services/utilsServices.js
+++ src/services/utilsServices.js
@@ -1,15 +1,16 @@
 import { BASE_URL } from "../utils/service";
+import { getConfigurationID } from "../utils/storage";
 
 export default class utilsService {
 
     async getAllProcedures() {
-        const response = await fetch(`${BASE_URL}/filtro/procedimientos`);
+        const response = await fetch(`${BASE_URL}/filtro/procedimientos?configurationId=${getConfigurationID()}`);
         const res = await response.json();
         return res;
     }
 
     async getAllVariables() {
-        const response = await fetch(`${BASE_URL}/filtro/variables`);
+        const response = await fetch(`${BASE_URL}/filtro/variables?configurationId=${getConfigurationID()}`);
         const res = await response.json();
         return res;
     }
